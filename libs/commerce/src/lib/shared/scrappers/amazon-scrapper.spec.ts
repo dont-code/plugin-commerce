acc87657041c84914ea96ee8ba5dde299785751d
Gérard Collin <gerard.collin@appliweb.net>
2023-03-29 20:09:32 +0200

    Fixed Amazon prices

--- libs/commerce/src/lib/shared/scrappers/amazon-scrapper.spec.ts
+++ libs/commerce/src/lib/shared/scrappers/amazon-scrapper.spec.ts
@@ -28,11 +28,11 @@ describe('AmazonScrapper', () => {
 
   it('should search', (done) => {
     expect(component).toBeTruthy();
-    component.searchProductsForNameOrId("Doro 8080 smartphone", false).then(value => {
+    component.searchProductsForNameOrId("téléphone Doro 8100", false).then(value => {
       expect(value.length>0).toBeTruthy();
-      expect(value[4].productName).toEqual("Doro 8080 Smartphone 4G Débloqué pour Seniors avec Écran de 5.7&quot;, Caméra de 16MP, Touche d'Assistance avec Géolocalisation...".substring(0,60));
-      expect(value[4].productPrice).toEqual (232);
-      expect(value[4].productId).toEqual("B07XLV8VW9");
+      expect(value[0].productName).toEqual("Doro 8100 4G Smartphone Débloqué pour Seniors, Résistant à l".substring(0,60));
+      expect(value[0].productPrice).toEqual (199);
+      expect(value[0].productId).toEqual("B09S3XTSPT");
       done();
     }).catch(error => {
       done (error);
@@ -43,10 +43,10 @@ describe('AmazonScrapper', () => {
   it('should get price', (done) => {
     expect(component).toBeTruthy();
     component.updatePrice({
-      productId:"B07XLV8VW9",
-      productName:"Doro 8080 Smartphone 4G Débloqué pour Seniors avec Écran"
+      productId:"B09S3XTSPT",
+      productName:"Doro 8100 4G Smartphone Débloqué"
     }).then(value => {
-      expect(value?.productPrice).toEqual(232);
+      expect(value?.productPrice).toEqual(199);
       expect(value?.currencyCode).toEqual("EUR");
       done();
     }).catch(error => {
