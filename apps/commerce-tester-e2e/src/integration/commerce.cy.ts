d607a0368b138c8d6f00e7410d112ed035c5bf17
Gérard Collin <gerard.collin@appliweb.net>
2023-08-26 10:51:09 +0200

    Fixed IndexedDB issues with E2E test

--- apps/commerce-tester-e2e/src/integration/commerce.cy.ts
+++ apps/commerce-tester-e2e/src/integration/commerce.cy.ts
@@ -14,169 +14,167 @@ import {getPriceFormWithName, getTableHeader} from "../support/commerce.po";
 
 describe('Commerce test', () => {
   beforeEach(() => cy.visit('/'));
-
+  afterEach(() => cy.window().then (win =>cy.forceDeleteIndexedDbStorage("Commerce Plugin Tester", win)));
 
   it('should display Price', () => {
-    cy.clearDbCollection("Online Shop").then (() => {
-      cy.clearDbCollection("Product"). then (() => {
+    cy.clearDbCollections("Commerce Plugin Tester", "Online Shop", "Product").then (() => {
 
-        cy.intercept('GET', '/assets/dev/templates.json').as('LoadTemplate');
-        cy.intercept({hostname:'corsproxy.io'}, {
-          body: RESULT_QUERY
+      cy.intercept('GET', '/assets/dev/templates.json').as('LoadTemplate');
+      cy.intercept({hostname:'corsproxy.io'}, {
+        body: RESULT_QUERY
 
-        }).as('LoadPrice');
+      }).as('LoadPrice');
 
-        getSubMenuWithText('Dev').click(); // Move to dev page
+      getSubMenuWithText('Dev').click(); // Move to dev page
 
-        cy.wait('@LoadTemplate');
+      cy.wait('@LoadTemplate');
 
-        clickAutoComplete('template');
-        selectPopupChoiceWithText('Online Shops');
-        getSendButton().click();
+      clickAutoComplete('template');
+      selectPopupChoiceWithText('Online Shops');
+      getSendButton().click();
 
-        clickAutoComplete('template');
-        selectPopupChoiceWithText('Product Price');
-        getSendButton().click();
+      clickAutoComplete('template');
+      selectPopupChoiceWithText('Product Price');
+      getSendButton().click();
 
-        getSubMenuWithText('Online Shop').click();
-        cy.get('th[id="header-Shop"]');
-        getButtonWithName("new").should('be.enabled');
-        getButtonWithName("new").click();
-        getInputWithName('Shop').type("Shop 1");
-        getDropdownWithName('Type').click("right");
-        getDropdownListItemWithName('GreenWeez').click();
-        getButtonWithName("save").click();
-        cy.get('th[id="header-Shop"]');
+      getSubMenuWithText('Online Shop').click();
+      cy.get('th[id="header-Shop"]');
+      getButtonWithName("new").should('be.enabled');
+      getButtonWithName("new").click();
+      getInputWithName('Shop').type("Shop 1");
+      getDropdownWithName('Type').click("right");
+      getDropdownListItemWithName('GreenWeez').click();
+      getButtonWithName("save").click();
+      cy.get('th[id="header-Shop"]');
 
-        getSubMenuWithText('Online Shop').click();  // Click again as it seems sometimes the new button is not working.
-        getButtonWithName("new").should('be.enabled');
-        getButtonWithName("new").click();
-        getInputWithName('Shop').type("Shop 2");
-        getDropdownWithName('Type').click("right");
-        getDropdownListItemWithName('EasyParapharmacie').click();
-        getButtonWithName("save").click();
+      getSubMenuWithText('Online Shop').click();  // Click again as it seems sometimes the new button is not working.
+      getButtonWithName("new").should('be.enabled');
+      getButtonWithName("new").click();
+      getInputWithName('Shop').type("Shop 2");
+      getDropdownWithName('Type').click("right");
+      getDropdownListItemWithName('EasyParapharmacie').click();
+      getButtonWithName("save").click();
 
-        cy.get('th[id="header-Shop"]');
-        getSubMenuWithText('Dev').click(); // Move to dev page
-        getSubMenuWithText('Online Shop').click();
-        cy.get('th[id="header-Shop"]');
-        getSubMenuWithText('Product').click();
+      cy.get('th[id="header-Shop"]');
+      getSubMenuWithText('Dev').click(); // Move to dev page
+      getSubMenuWithText('Online Shop').click();
+      cy.get('th[id="header-Shop"]');
+      getSubMenuWithText('Product').click();
 
-        getButtonWithName("new").should('be.enabled');
-        getButtonWithName("new").click();
-        getInputWithName('Name').type("Product 1");
-        getInputWithName('nameInShop').type("Product 1 Name");
-        getDropdownWithName('shop').click('right');
-        getDropdownListItemWithName('Shop 2').click();
-        getButtonWithName('FetchPrice').click();
-        cy.wait('@LoadPrice');
+      getButtonWithName("new").should('be.enabled');
+      getButtonWithName("new").click();
+      getInputWithName('Name').type("Product 1");
+      getInputWithName('nameInShop').type("Product 1 Name");
+      getDropdownWithName('shop').click('right');
+      getDropdownListItemWithName('Shop 2').click();
+      getButtonWithName('FetchPrice').click();
+      cy.wait('@LoadPrice');
 
-        getButtonWithText('Select').first().click();
-        checkValueOfInputWithName('cost', Intl.NumberFormat(navigator.language).format(10.99));
+      getButtonWithText('Select').first().click();
+      checkValueOfInputWithName('cost', Intl.NumberFormat(navigator.language).format(10.99));
 
-        getDropdownWithName('currencyCode').click('right');
-        getDropdownListItemWithName('Pound Sterling - GBP').click();
+      getDropdownWithName('currencyCode').click('right');
+      getDropdownListItemWithName('Pound Sterling - GBP').click();
 
-        getButtonWithName("save").click();
+      getButtonWithName("save").click();
 
-        getListRowWithText(FormatUtils.generateMoney(10.99, "GBP"));
-      });
+      getListRowWithText(FormatUtils.generateMoney(10.99, "GBP"));
 
     });
   });
 
   it('should be easy to use', () => {
-    cy.clearDbCollection("Online Shop").then (() => {
-      cy.clearDbCollection("Multi Product").then(() => {
-          // Loads a specific template
-        cy.intercept('GET', '/assets/dev/templates.json', MULTI_PRODUCT_TEMPLATE
-        ).as('LoadMultiTemplate');
+    cy.clearDbCollections('Commerce Plugin Tester',"Online Shop", "Multi Product").then( () => {
+        // Loads a specific template
+      cy.intercept('GET', '/assets/dev/templates.json', MULTI_PRODUCT_TEMPLATE
+      ).as('LoadMultiTemplate');
 
-        getSubMenuWithText('Dev').click(); // Move to dev page
+      getSubMenuWithText('Dev').click(); // Move to dev page
 
-        cy.wait('@LoadMultiTemplate');
+      cy.wait('@LoadMultiTemplate');
 
-        clickAutoComplete('template');
-        selectPopupChoiceWithText('Online Shop');
-        getSendButton().click();
+      clickAutoComplete('template');
+      selectPopupChoiceWithText('Online Shop');
+      getSendButton().click();
 
-        clickAutoComplete('template');
-        selectPopupChoiceWithText('Multi Product');
-        getSendButton().click();
+      clickAutoComplete('template');
+      selectPopupChoiceWithText('Multi Product');
+      getSendButton().click();
 
-        getSubMenuWithText('Online Shop').click();
-        getTableHeader('Shop').should('contain.text', "Shop");
+      getSubMenuWithText('Online Shop').click();
+      getTableHeader('Shop').should('contain.text', "Shop");
 
-        getButtonWithName("new").should('be.enabled');
-        getButtonWithName("new").click();
-        getInputWithName('Shop').type("Shop GW");
-        getDropdownWithName('Type').click("right");
-        getDropdownListItemWithName('GreenWeez').click();
-        getButtonWithName("save").click();
-        getListRowWithText("Shop GW");
+      getButtonWithName("new").should('be.enabled');
+      getButtonWithName("new").click();
+      getInputWithName('Shop').type("Shop GW");
+      getDropdownWithName('Type').click("right");
+      getDropdownListItemWithName('GreenWeez').click();
+      getButtonWithName("save").click();
+      getListRowWithText("Shop GW");
 
-        getSubMenuWithText('Online Shop').click();  // Click again as it seems sometimes the new button is not working.
-        getTableHeader('Shop').should('contain.text', "Shop");
+      getSubMenuWithText('Online Shop').click();  // Click again as it seems sometimes the new button is not working.
+      getTableHeader('Shop').should('contain.text', "Shop");
 
-        getButtonWithName("new").should('be.enabled');
-        getButtonWithName("new").click();
-        getInputWithName('Shop').type("Shop EP");
-        getDropdownWithName('Type').click("right");
-        getDropdownListItemWithName('EasyParapharmacie').click();
-        getButtonWithName("save").click();
-        getListRowWithText("Shop EP");
+      getButtonWithName("new").should('be.enabled');
+      getButtonWithName("new").click();
+      getInputWithName('Shop').type("Shop EP");
+      getDropdownWithName('Type').click("right");
+      getDropdownListItemWithName('EasyParapharmacie').click();
+      getButtonWithName("save").click();
+      getListRowWithText("Shop EP");
 
-        getSubMenuWithText('Multi Product').click();
-        getTableHeader('Name').should('contain.text', "Name");
+      getSubMenuWithText('Multi Product').click();
+      getTableHeader('Name').should('contain.text', "Name");
 
-        getButtonWithName("new").should('be.enabled');
-        getButtonWithName("new").click();
-        getInputWithName('Name').type("Global Product Name");
+      getButtonWithName("new").should('be.enabled');
+      getButtonWithName("new").click();
+      getInputWithName('Name').type("Global Product Name");
 
-        let curShop = getPriceFormWithName("Shop EP");
-        curShop.within(() => {
-          getInputWithName('nameInShop').focus();
-          getInputWithName('nameInShop').should('have.value', "Global Product Name");
-          checkValueOfDropdownWithName('shop', "Shop EP");
-        });
-
-        curShop = getPriceFormWithName("Shop GW");
-        curShop.within(() => {
-          getInputWithName('nameInShop').should('have.value', "Global Product Name");
-          checkValueOfDropdownWithName('shop', "Shop GW");
-        });
-
-        getButtonWithName("save").click();
-        cy.get('th[id="header-Name"]');
-
-        getSubMenuWithText('Dev').click(); // Move to dev page
-        getSubMenuWithText('Multi Product').click();
-
-        getListRowWithText("Global Product Name").click();
-
-        // Check the values were saved properly
-        curShop = getPriceFormWithName("Shop EP");
-        curShop.within(() => {
-          getInputWithName('nameInShop').should('have.value', "Global Product Name");
-          checkValueOfDropdownWithName('shop', "Shop EP");
-        });
-
-        getInputWithName('Name').type(" New");
-
-        curShop = getPriceFormWithName("Shop EP");
-        curShop.within(() => {
-          getInputWithName('nameInShop').focus();
-          getInputWithName('nameInShop').should('have.value', "Global Product Name New");
-
-          getInputWithName('nameInShop').clear().type("Shop Product Name");
-        });
-
-        getInputWithName('Name').type(" Again");
-        curShop.within(() => {
-          getInputWithName('nameInShop').focus();
-          getInputWithName('nameInShop').should('have.value', "Shop Product Name");
-        });
+      let curShop = getPriceFormWithName("Shop EP");
+      curShop.within(() => {
+        getInputWithName('nameInShop').focus();
+        getInputWithName('nameInShop').should('have.value', "Global Product Name");
+        checkValueOfDropdownWithName('shop', "Shop EP");
       });
+
+      curShop = getPriceFormWithName("Shop GW");
+      curShop.within(() => {
+        getInputWithName('nameInShop').should('have.value', "Global Product Name");
+        checkValueOfDropdownWithName('shop', "Shop GW");
+      });
+
+      getButtonWithName("save").click();
+      cy.get('th[id="header-Name"]');
+
+      getSubMenuWithText('Dev').click(); // Move to dev page
+      getSubMenuWithText('Multi Product').click();
+
+      getListRowWithText("Global Product Name").click();
+
+      // Check the values were saved properly
+      curShop = getPriceFormWithName("Shop EP");
+      curShop.within(() => {
+        getInputWithName('nameInShop').should('have.value', "Global Product Name");
+        checkValueOfDropdownWithName('shop', "Shop EP");
+      });
+
+      getInputWithName('Name').type(" New");
+
+      curShop = getPriceFormWithName("Shop EP");
+      curShop.within(() => {
+        getInputWithName('nameInShop').focus();
+        getInputWithName('nameInShop').should('have.value', "Global Product Name New");
+
+        getInputWithName('nameInShop').clear().type("Shop Product Name");
+      });
+
+      getInputWithName('Name').type(" Again");
+      curShop.within(() => {
+        getInputWithName('nameInShop').focus();
+        getInputWithName('nameInShop').should('have.value', "Shop Product Name");
+      });
+    }, (error) => {
+      throw new Error (error);
     });
   });
 
