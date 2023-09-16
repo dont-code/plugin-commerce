a9c4ce422ae06961edeebb77e10d78f880c0c18f
Gérard Collin <gerard.collin@appliweb.net>
2023-09-16 11:46:59 +0200

    20s timeout, sometimes github action is really slow

--- apps/commerce-tester-e2e/cypress.config.js
+++ apps/commerce-tester-e2e/cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
   "videosFolder": "../../dist/cypress/apps/commerce-tester-e2e/videos",
   "screenshotsFolder": "../../dist/cypress/apps/commerce-tester-e2e/screenshots",
   "chromeWebSecurity": false,
-  "defaultCommandTimeout": 10000,
+  "defaultCommandTimeout": 20000,
   e2e: {
     setupNodeEvents(on, config) {},
     specPattern: './src/integration/**/*.cy.{js,jsx,ts,tsx}',
